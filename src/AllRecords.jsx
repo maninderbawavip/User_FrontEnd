e2fd675a8711fb189d28b1cd68e2a30152ab814b
Maninder Singh Bawa <maninderbawavip@gmail.com>
2021-08-05 13:44:34 +0530

    fixed few things

--- src/AllRecords.jsx
+++ src/AllRecords.jsx
@@ -1,58 +1,53 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function AllRecords({ data }) {
-    const [ ingData , setIngData] = useState('')
-    axios.get('http://localhost:5000/Img')
-        .then((res) => {
-            console.log("res.data._id:", res.data.data)
-            setIngData(res.data.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+
+    const [allUsers, setUsersData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/users')
+            .then((res) => {
+                console.log(res.data)
+                setUsersData(res.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
     return (
 
         <div className="">
-            {
-                data != '' ? (
-                    <div className="">
-                        <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                            {data.data.map((person) => (
-                                <li
-                                    key={person.email}
-                                    className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
-                                >
-                                    <div className="flex-1 flex flex-col p-8">
-                                        <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src={person} alt="" />
-                                        <h3 className="mt-6 text-gray-900 text-sm font-medium">{person.FullName}</h3>
-                                        <dl className="mt-1 flex-grow flex flex-col justify-between">
-                                            <dt className="sr-only">Title</dt>
-                                            <dd className="text-gray-500 text-sm">{person.userName}</dd>
-                                            <dd className="text-gray-500 text-sm">{person.phone}</dd>
-                                            <dd className="text-gray-500 text-sm">{person.zipCode}</dd>
-                                            <dt className="sr-only">Role</dt>
-                                            <dd className="mt-3">
-                                                <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                                                    {person.email}
-                                                </span>
-                                            </dd>
-                                        </dl>
-                                    </div>
-                                    <div>
 
-                                    </div>
-                                </li>
-                            ))}
+            <div className="">
+                <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {allUsers.map((person, index) => (
+                        <li
+                            key={index}
+                            className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+                        >
+                            <div className="flex-1 flex flex-col p-8">
+                                <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src={`http://localhost:5000/static/${person.profileImage.filename}`} alt="" />
+                                <h3 className="mt-6 text-gray-900 text-sm font-medium">{person.FullName}</h3>
+                                <dl className="mt-1 flex-grow flex flex-col justify-between">
+                                    <dt className="sr-only">Title</dt>
+                                    <dd className="text-gray-500 text-sm">{person.userName}</dd>
+                                    <dd className="text-gray-500 text-sm">{person.zipCode}</dd>
+                                </dl>
+                            </div>
+                            <div>
 
-                        </ul>
+                            </div>
+                        </li>
+                    ))}
 
-                    </div>
-                ) : ''
-            }
+                </ul>
+
+            </div>
+
+
         </div>
     )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -7,24 +7,16 @@ export default function Register() {
 
     const histroy = useHistory()
     const [error, setError] = useState('')
-    const [succes, setSucces] = useState('')
-    const [selectedFile, setSelectedFile] = useState('')
-    const [selectedFileRefrence, setSelectedFileRefrence] = useState('')
-    console.log("------>",selectedFile)
+    const [errors, setErrors] = useState([])
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [selectedFileRefrence, setSelectedFileRefrence] = useState(null)
 
-    const handleSubmit = (e) => {
-        let data = {}
-        e.preventDefault();
-        const formInputData = new FormData(e.currentTarget);
-        for (let [key, value] of formInputData.entries()) {
-            data[key] = value
-        }
 
-        const formImgData =new FormData();
-        formImgData.append('file', selectedFile)
-
-        if (data.password === data.Confirmpassword) {
-            axios.post('http://localhost:5000/Img', formImgData)
+    const handleFile = (file) => {
+        const formImgData = new FormData();
+        formImgData.append('file', file)
+        setSelectedFile(URL.createObjectURL(file))
+        axios.post('http://localhost:5000/userImage', formImgData)
             .then((res) => {
                 console.log("res.data._id:", res)
                 setSelectedFileRefrence(res.data.data._id)
@@ -32,37 +24,77 @@ export default function Register() {
             .catch((err) => {
                 console.log(err)
             })
+    }
 
+    const getErrorsIfAny = (data) => {
+        let errors = [];
+        if (selectedFileRefrence === null) {
+            errors.push('Please select profile pic')
+        }
+        if (data.password !== data.Confirmpassword) {
+            errors.push('Passwords Do Not Match!!!')
+        }
+        if (data.FullName.length < 4) {
+            errors.push('Name must be greater than 3 digits')
+        }
+        return errors
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let data = {}
+        const formInputData = new FormData(e.currentTarget);
+        for (let [key, value] of formInputData.entries()) {
+            data[key] = value
+        }
+
+        let errors = getErrorsIfAny(data)
+        setErrors(errors)
+
+        if (errors.length === 0) {
             const finalData = {
-                FullName:data.FullName,
-                userName:data.userName,
-                email:data.email,
-                phone:data.phone,
-                password:data.password,
-                zipCode:data.zipCode,
-                profileImage:selectedFileRefrence,
+                FullName: data.FullName,
+                userName: data.userName,
+                password: data.password,
+                zipCode: data.zipCode,
+                profileImage: selectedFileRefrence,
             }
-            axios.post('http://localhost:5000/', finalData)
+            axios.post('http://localhost:5000/users', finalData)
                 .then((res) => {
-                    setSucces('data insrted!')
-                    console.log(res)
                     histroy.push("/")
                 })
                 .catch((err) => {
                     console.log(err)
-                    setError('Enter all fields in proper way')
+                    setError('Some Error occured')
                 })
         }
-        // console.log('value', data)
     }
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Register</h2>
-                <img src={selectedFile} alt="" />
             </div>
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+
+                    <div>
+                        {selectedFile && <img src={selectedFile} alt="" />}
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                            Profile Pic
+                        </label>
+                        <div className="mt-1">
+                            <input
+                                id="password"
+                                type="file"
+                                autoComplete="current-password"
+                                onChange={(e) => handleFile(e.target.files[0])}
+                                required
+                                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+
                     <form className="space-y-6" onSubmit={handleSubmit}>
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -94,38 +126,6 @@ export default function Register() {
                                 />
                             </div>
                         </div>
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                                Contact Number
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    id="email"
-                                    name="phone"
-                                    type="number"
-                                    autoComplete="email"
-                                    required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                                Email address
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
                         <div>
                             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                                 Password
@@ -172,35 +172,16 @@ export default function Register() {
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                                Profile Pic
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    id="password"
-                                    type="file"
-                                    autoComplete="current-password"
-                                    onChange={(e) => setSelectedFile(e.target.files[0])}
-                                    required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            {succes}
-                            {error}
+                            {<div className="text-red">{error}</div>}
+                            {errors.map(err => <div className="text-red-500">{err}</div>)}
                             <button
                                 type="submit"
                                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
-                                Sign in
+                                Register
                             </button>
                         </div>
                     </form>
-
-
-
                 </div>
             </div>
         </div>
